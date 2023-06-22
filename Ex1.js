a85be4512bb01e9907e41be8a38c78439bd440bb
Nguyễn Thăng Long <tuuyen19112911@gmail.com>
2023-06-22 19:00:13 +0700

    test
    test

---
+++ Ex1.js
@@ -0,0 +1,13 @@
+//     var thanhpho = "Cần Thơ";
+// var SinhVien = {
+//         name:"Nguyễn Thành Nhân",
+//         year:1993,
+//         city:thanhpho,  
+        function intro(){
+            var name = "Nguyễn Thành Nhân";
+            document.write(name); 
+            // document.write("- Sinh năm: " + this.year + "<br>"); 
+            // document.write("- Sống tại: " + this.city);
+        }
+    // }
+intro();
